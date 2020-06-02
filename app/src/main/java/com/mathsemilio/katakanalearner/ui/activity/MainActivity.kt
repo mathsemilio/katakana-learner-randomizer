34cdcd4a27505a70a58e5a7c557dd8de85a0d4cc
mathsemilio <mathsemilio@gmail.com>
2020-06-02 16:09:48 -0300

    Added support for dark mode within the MainActivity.kt, and small code rearrangement.

--- app/src/main/java/com/mathsemilio/katakanalearner/ui/activity/MainActivity.kt
+++ app/src/main/java/com/mathsemilio/katakanalearner/ui/activity/MainActivity.kt
@@ -1,16 +1,29 @@
-package com.mathsemilio.katakanalearner.activity
+package com.mathsemilio.katakanalearner.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.mathsemilio.katakanalearner.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    //==========================================================================================
-    // OnCreate
-    //==========================================================================================
+    private var darkModeActivated: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image_dark_mode_switch.setOnClickListener { switchDarkMode(darkModeActivated) }
+    }
+
+    private fun switchDarkMode(activated: Boolean) {
+        darkModeActivated = if (activated) {
+            AppCompatDelegate.MODE_NIGHT_NO
+            false
+        } else {
+            AppCompatDelegate.MODE_NIGHT_YES
+            true
+        }
     }
 }
