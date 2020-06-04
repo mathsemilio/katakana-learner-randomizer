8e084821fc585d9cac9c5a10fb90b7fb063d1b52
mathsemilio <mathsemilio@gmail.com>
2020-06-04 16:23:24 -0300

    Logic to activate/deactivate dark mode implemented.

--- app/src/main/java/com/mathsemilio/katakanalearner/ui/fragment/GameWelcomeScreen.kt
+++ app/src/main/java/com/mathsemilio/katakanalearner/ui/fragment/GameWelcomeScreen.kt
@@ -1,6 +1,7 @@
 package com.mathsemilio.katakanalearner.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,18 +9,32 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.mathsemilio.katakanalearner.R
 import com.mathsemilio.katakanalearner.databinding.GameWelcomeScreenBinding
+import com.mathsemilio.katakanalearner.util.DarkModeSelector
+
+private const val TAG_WELCOME_SCREEN = "GameWelcomeScreen"
 
 class GameWelcomeScreen : Fragment() {
-
-    private lateinit var binding: GameWelcomeScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = GameWelcomeScreenBinding.inflate(inflater, container, false)
+        val binding: GameWelcomeScreenBinding =
+            GameWelcomeScreenBinding.inflate(inflater, container, false)
 
-        binding.buttonPlay.setOnClickListener { navigateToMainScreen() }
+        binding.buttonStart.setOnClickListener { navigateToMainScreen() }
+
+        binding.darkModeSwitch.isChecked = DarkModeSelector.isActivated
+
+        binding.darkModeSwitch.setOnCheckedChangeListener { buttonView, _ ->
+            if (buttonView.isChecked) {
+                Log.i(TAG_WELCOME_SCREEN, "onCreateView: Dark mode on")
+                DarkModeSelector.switchDarkModeState()
+            } else {
+                Log.i(TAG_WELCOME_SCREEN, "onCreateView: Dark mode off")
+                DarkModeSelector.switchDarkModeState()
+            }
+        }
 
         return binding.root
     }
