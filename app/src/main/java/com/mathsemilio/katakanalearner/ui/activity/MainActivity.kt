13054e940b781d95aee9eb07ccc4bf24810846a5
mathsemilio <mathsemilio@gmail.com>
2020-06-04 16:20:20 -0300

    Dark mode logic removed from the MainActivity.kt.

--- app/src/main/java/com/mathsemilio/katakanalearner/ui/activity/MainActivity.kt
+++ app/src/main/java/com/mathsemilio/katakanalearner/ui/activity/MainActivity.kt
@@ -2,28 +2,12 @@ package com.mathsemilio.katakanalearner.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import com.mathsemilio.katakanalearner.R
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var darkModeActivated: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        image_dark_mode_switch.setOnClickListener { switchDarkMode() }
-    }
-
-    private fun switchDarkMode() {
-        darkModeActivated = if (darkModeActivated) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            false
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            true
-        }
     }
 }
